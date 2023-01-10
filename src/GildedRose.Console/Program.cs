d51733b8c35bac466bfc595488aced2f69b24afb
Victor Cantizano <victor.cantizano@techsbcn.com>
2023-01-10 09:17:02 +0100

    Fixing correct commit

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -3,13 +3,11 @@ using GildedRose.Data.Repositories;
 using GildedRose.Entities;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Collections.Generic;
 
 namespace GildedRose.Console
 {
     public class Program
     {
-        public IList<Item> Items;
         static void Main(string[] args)
         {
             System.Console.WriteLine("OMGHAI!");
@@ -24,7 +22,6 @@ namespace GildedRose.Console
             storeService.UpdateItems();
 
             System.Console.ReadKey();
-
         }
 
         private static void SetupDI(IServiceCollection services)
@@ -33,14 +30,6 @@ namespace GildedRose.Console
             services.AddScoped<IItemService<ExpiringItem>, ExpiringItemService>();
             services.AddScoped<IStoreService, StoreService>();
             services.AddScoped<IItemRepository, ItemRepository>();
-        }
-
-        public class Item
-        {
-            public string Name { get; set; }
-            public int SellIn { get; set; }
-            public int Quality { get; set; }
-            public decimal Price { get; set; }
         }
     }
 }
