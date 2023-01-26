a63430f391783988e335b6abca9a709e1c8b45f5
josemiguel.calvente <josemiguel.calvente@veepee.com>
2023-01-26 14:25:50 +0100

    Up changes JMCD

--- src/GildedRose.Domain/Models/Item.cs
+++ src/GildedRose.Domain/Models/Item.cs
@@ -4,7 +4,7 @@ namespace GildedRose.Domain.Models
 {
     public class Item
     {
-        public string Name { get; init; } = string.Empty;
+        public string Name { get; init; } = default!;
         public int SellIn { get; set; } = default!;
         public int Quality { get; set; } = default!;
         public decimal Price { get; set; } = default!;
